2c83b2686bfcd4ee9ac07d6c140820fa39396316
AasthaSingh053 <aasthasinghthakur2007@gmail.com>
2026-02-05 20:50:02 +0530

    Updating Dsa practice

---
+++ RotateMatrixBy90.java
@@ -0,0 +1,31 @@
+import java.util.*;
+class Solution {
+    public void rotate(int[][] matrix) {
+        int n = matrix.length;
+        //Create its transpose
+        for(int i = 0;i<n;i++){
+            for(int j = i+1;j<n;j++){
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+        //Now Reverse the matrix
+        for(int i = 0;i<n;i++){
+            for(int j = 0;j<n/2;j++){
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[i][n-j-1];
+                matrix[i][n-j-1] = temp;
+            }
+        }
+        System.out.println(Arrays.toString(matrix));
+
+    }
+}
+public class RotateMatrixBy90 {
+    public static void main(String args[]){
+        int[][] arr = new int[][] {{1,2,3},{4,5,6},{7,8,9}};
+        Solution obj = new Solution();
+        obj.rotate(arr);
+    }
+}
